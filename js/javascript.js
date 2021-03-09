ac24e20770c65f86092633fc17e0c6087920c966
diegojavierconchillowagner <Diegoo.javier.wagner@gmail.com>
2021-03-09 13:59:05 -0300

    Juego de cofla

---
+++ js/javascript.js
@@ -0,0 +1,75 @@
+//Soluciones cap 1 / Cofla 1
+dineroCof = prompt("cuanto dinero tienes cofla ?");
+dineroRob = prompt("cuanto dinero tienes Roberto ?");
+dineroPed = prompt("cuanto dinero tienes Pedro ?");
+
+if (dineroCof >= 0.6 && dineroCof < 1) {
+    alert('cofla ,comprate el helado de agua')
+}
+
+else if (dineroCof >= 1 && dineroCof < 1.6){
+    alert('cofla ,comprate el helado de crema')
+}
+else if (dineroCof >= 1.6 && dineroCof < 1.7){
+    alert('cofla ,comprate el helado de heladix')
+}
+else if (dineroCof >= 1.7 && dineroCof < 1.8){
+    alert('cofla ,comprate el helado de heladivich')
+}
+else if (dineroCof >= 1.8 && dineroCof < 2.9){
+    alert('cofla ,comprate el helado de helardo')
+}
+else if (dineroCof >= 2.9){
+    alert('cofla ,helado con confites o pote de 1/4kg');
+}
+else {
+    alert ('cofla ,pobre de mierda')
+}
+
+//
+if (dineroRob >= 0.6 && dineroRob < 1) {
+    alert('roberto ,comprate el helado de agua')
+}
+
+else if (dineroRob >= 1 && dineroRob < 1.6){
+    alert('roberto ,comprate el helado de crema')
+}
+else if (dineroRob >= 1.6 && dineroRob < 1.7){
+    alert('roberto ,comprate el helado de heladix')
+}
+else if (dineroRob >= 1.7 && dineroRob < 1.8){
+    alert('roberto ,comprate el helado de heladivich')
+}
+else if (dineroRob >= 1.8 && dineroRob < 2.9){
+    alert('roberto ,comprate el helado de helardo')
+}
+else if (dineroRob >= 2.9){
+    alert('roberto ,helado con confites o pote de 1/4kg');
+}
+else {
+    alert ('roberto ,pobre de mierda')
+}
+//
+//
+if (dineroPed >= 0.6 && dineroPed < 1) {
+    alert('pedro ,comprate el helado de agua')
+}
+
+else if (dineroPed >= 1 && dineroPed < 1.6){
+    alert('pedro ,comprate el helado de crema')
+}
+else if (dineroPed >= 1.6 && dineroPed < 1.7){
+    alert('pedro ,comprate el helado de heladix')
+}
+else if (dineroPed >= 1.7 && dineroPed < 1.8){
+    alert('pedro ,comprate el helado de heladivich')
+}
+else if (dineroPed >= 1.8 && dineroPed < 2.9){
+    alert('pedro ,comprate el helado de helardo')
+}
+else if (dineroPed >= 2.9){
+    alert('pedro ,helado con confites o pote de 1/4kg');
+}
+else {
+    alert ('pedro ,pobre de mierda')
+}
